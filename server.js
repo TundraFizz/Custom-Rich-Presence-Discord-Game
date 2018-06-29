8a84b04a8d346c05798276ef5f59024a1358780e
Tundra Fizz <coleman.leif@gmail.com>
2018-06-29 11:26:39 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("ejs");
 var fs         = require("fs");
+var readline   = require("readline");
 var yaml       = require("js-yaml");
 var express    = require("express");         // Define the web framework
 var bodyParser = require("body-parser");     // Allows you to read POST data
@@ -8,24 +9,16 @@ app.set("views", "./views");                 // Define the views directory
 app.use(express.static("./static"));         // Define the static directory
 app.use(bodyParser.urlencoded({extended: true})); // Setting for bodyParser
 
-fs.readFileSync("./views/index.ejs"     , "utf-8");
-fs.readFileSync("./static/css/index.css", "utf-8");
-fs.readFileSync("./static/img/icon.jpg" , "utf-8");
-fs.readFileSync("./static/js/index.js"  , "utf-8");
-fs.readFileSync("./static/config.yml"   , "utf-8");
-
 fs.access("config.yml", function(err){
   // Check to see if config.yml already file exists for the application to read data from
   // If it doesn't exit, copy from the default configuration file and read that instead
   if(err){
-    var rs = fs.createReadStream("./static/config.yml");
-    var ws = fs.createWriteStream("./config.yml");
-    rs.pipe(ws);
-    ws.on("close", function(){
+    fs.writeFile("config.yml", fs.readFileSync("./static/config.yml", "utf-8"), function(err){
       StartServer();
     });
-  }else
+  }else{
     StartServer();
+  }
 });
 
 function StartServer(){
