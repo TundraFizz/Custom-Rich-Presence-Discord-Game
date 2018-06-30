dd5b9a897e65a48f61a0b7f89371af9a91373ca2
Tundra Fizz <MageLeif@Yahoo.com>
2018-06-30 01:46:00 -0700

    Update build.js

--- build/build.js
+++ build/build.js
@@ -2,8 +2,10 @@ var nexe = require("nexe");
 
 nexe.compile({
   output: "yolo-swag",
-  target: "windows-x86-9.11.1",
+  // target: "windows-x86-8.11.3",
+  // target: "win32-x86-8.11.3",
+  target: "win32-x64-9.11.1",
   resources: ["./views/*.ejs", "./static/**/*"],
-  ico: "./build/icon.png",
+  ico: "./build/icon.ico",
   build: true
 });
